d19137a8a938b1e85fe721bd17ecb64f49707a19
mo-jo-dev <b210055@nitsikkim.ac.in>
2024-05-29 16:12:00 +0530

    added main linkedin file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const startServer = async () => {
-    const res = axios.get('https://www.amazon.in/ASUS-i7-11800H-GeForce-Windows-FX506HE-HN382W/dp/B0CCYGC3TS/ref=sr_1_30?crid=2K4BZ3OO7TW7L&keywords=laptop&psr=EY17&qid=1702997208&refinements=p_36%3A7252032031&rnid=7252027031&s=todays-deals&sprefix=lapto%2Ctodays-deals%2C548&sr=1-30&th=1');
+    const res = await axios.get('https://www.linkedin.com/jobs/collections/');
     console.log(res);
     app.listen(PORT,() => {
         console.log(`SERVER STARTED IN PORT: ${PORT}`);
